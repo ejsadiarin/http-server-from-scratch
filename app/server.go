e315b3435ff7a6f4f7df1ac99425ef76d241e35d
ejsadiarin <kyube.ejs@proton.me>
2024-12-28 00:35:50 +0800

    feat: read header

--- app/server.go
+++ app/server.go
@@ -13,16 +13,41 @@ var (
 	_ = os.Exit
 )
 
-func echoHandler(urlPath string) string {
+func echoHandler(urlPath string) (string, error) {
 	str := strings.Split(urlPath, "/")
 	// always expect an arg in url (/echo/<arg>)
 	if len(str) < 3 {
 		fmt.Println("Missing string argument.")
-		return "HTTP/1.1 404 Not Found\r\n\r\n"
+		return "HTTP/1.1 404 Not Found\r\n\r\n", fmt.Errorf("Missing string argument")
 	}
 	arg := str[2]
 	response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(arg), arg)
-	return response
+	return response, nil
+}
+
+func userAgentHandler(req []byte) (string, error) {
+	if !strings.Contains(string(req), "User-Agent") {
+		// return "HTTP/1.1 404 Not Found\r\n\r\n"
+		return "HTTP/1.1 404 Not Found\r\n\r\n User-Agent not found\r\n", fmt.Errorf("No User-Agent header found.")
+	}
+	lines := strings.Split(string(req), "\r\n")
+	fmt.Printf("Lines (len %d): %v\n", len(lines), lines)
+	for i, line := range lines {
+		fmt.Printf("%d: %s\n", i, line)
+	}
+	var idx int
+	for i, v := range lines {
+		if strings.Contains(v, "User-Agent") {
+			idx = i
+		}
+	}
+	UALine := lines[idx]
+	UASlice := strings.Split(UALine, ":")
+	UAValue := strings.TrimSuffix(UASlice[1], "\r\n")
+	UAValue = strings.TrimSpace(UAValue)
+	fmt.Println(UAValue)
+	response := fmt.Sprintf("HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(UAValue), UAValue)
+	return response, nil
 }
 
 func main() {
@@ -49,7 +74,6 @@ func main() {
 	conn.Read(req)
 	fmt.Println(string(req))
 
-	// custom mux
 	// parse the url path
 	lines := strings.Split(string(req), "\r\n")
 	if len(lines) < 1 {
@@ -77,13 +101,24 @@ func main() {
 	// 	os.Exit(1)
 	// }
 
+	// custom mux
 	var response string
 	if urlPath == "/" {
 		response = "HTTP/1.1 200 OK\r\n\r\n"
 	} else if strings.Contains(urlPath, "/echo") {
 		fmt.Println("Contains /echo")
 		// echo handler logic here
-		response = echoHandler(urlPath)
+		response, err = echoHandler(urlPath)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	} else if urlPath == "/user-agent" {
+		response, err = userAgentHandler(req)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	} else {
 		response = "HTTP/1.1 404 Not Found\r\n\r\n"
 	}
